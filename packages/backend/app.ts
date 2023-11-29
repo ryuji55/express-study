aa36375dc04c90f14a4564fcf7c6e3f5652593f3
ryuji55 <rt.october.2@icloud.com>
2023-11-29 22:52:53 +0900

    Fix:port番号を環境変数に

--- packages/backend/app.ts
+++ packages/backend/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Example app listening on port 3000!");
 });
