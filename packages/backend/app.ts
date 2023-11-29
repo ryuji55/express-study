c9e39a44e17fafbb2442ad9791393be9a1f2bd31
ryuji55 <rt.october.2@icloud.com>
2023-11-30 08:40:02 +0900

    Add:redis導入

--- packages/backend/app.ts
+++ packages/backend/app.ts
@@ -1,14 +1,34 @@
 import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
+import Redis from "ioredis";
 
 dotenv.config();
 
 const app = express();
 
+const redis = new Redis({
+  port: Number(process.env.REDIS_PORT),
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASSWORD,
+  enableOfflineQueue: false,
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Example app listening on port 3000!");
+redis.once("ready", () => {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log("Example app listening on port 3000!");
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
+
+redis.on("error", (err: Error) => {
+  console.error(err);
+  process.exit(1);
 });
