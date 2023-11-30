272432768adb2aac9002256e678f7b8ba36b0c48
ryuji55 <rt.october.2@icloud.com>
2023-12-01 08:32:13 +0900

    Fix

--- packages/backend/app.ts
+++ packages/backend/app.ts
@@ -24,12 +24,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const userId = req.params.id;
+  try {
+    const user = await redis.get(`users:${userId}`);
+    if (user) {
+      res.status(200).json(JSON.parse(user));
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
 redis.once("ready", async () => {
-  console.log("object");
   try {
     await init();
     const result = await redis.get("users:1");
-    console.log(result);
     app.listen(process.env.PORT, () => {
       console.log("Example app listening on port 3000!");
     });
